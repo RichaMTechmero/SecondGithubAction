23415159d7d3142180df976f1dd0ab2f187ca6e7
RichaMTechmero <148053261+RichaMTechmero@users.noreply.github.com>
2023-10-27 11:43:16 +0530

    26

--- Pods/Target Support Files/Pods-GitTetsingDemo-GitTetsingDemoUITests/Pods-GitTetsingDemo-GitTetsingDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GitTetsingDemo_GitTetsingDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_GitTetsingDemo_GitTetsingDemoUITests
-@end
